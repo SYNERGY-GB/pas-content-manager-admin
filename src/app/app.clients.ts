1686fe0c78a25d52075a3974b9fee0e14dca0f8e
Leonardo Antonio Santella <leocaracas2010@gmail.com>
2017-01-12 16:30:54 -0400

    login template changed

---
+++ src/app/app.clients.ts
@@ -0,0 +1,23 @@
+export const CLIENTS = [
+    {
+        name: "client_1",
+        credentials: {
+            apiKey: "AIzaSyDslR7pW38YJMMhTxiElsRbFQhavQGsPqA",
+            authDomain: "client2-a188f.firebaseapp.com",
+            databaseURL: "https://client2-a188f.firebaseio.com",
+            storageBucket: "client2-a188f.appspot.com",
+            messagingSenderId: "1069202161142"
+        }
+    },
+    {
+        name: "client_2",
+        credentials: {
+            apiKey: "AIzaSyDslR7pW38YJMMhTxiElsRbFQhavQGsPqA",
+            authDomain: "client2-a188f.firebaseapp.com",
+            databaseURL: "https://client2-a188f.firebaseio.com",
+            storageBucket: "client2-a188f.appspot.com",
+            messagingSenderId: "1069202161142"
+        }
+    }
+  
+]
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import {Component, ViewEncapsulation} from '@angular/core';
 import {FormGroup, AbstractControl, FormBuilder, Validators} from '@angular/forms';
 import { AngularFire } from 'angularfire2';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { CLIENTS } from '../../app.clients.ts';
 
 @Component({
   selector: 'login',
@@ -15,27 +16,46 @@ export class Login {
   public email:AbstractControl;
   public password:AbstractControl;
   public submitted:boolean = false;
+  public client:AbstractControl;
+  public credentials:Object;
 
   constructor(fb:FormBuilder, private af: AngularFire, private router: Router) {
     this.form = fb.group({
       'email': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
-      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+      'password': ['', Validators.compose([Validators.required, Validators.minLength(4)])],
+      'client': ['', Validators.compose([Validators.required, Validators.minLength(3)])]
     });
 
     this.email = this.form.controls['email'];
     this.password = this.form.controls['password'];
+    this.client = this.form.controls['client'];
   }
 
   public onSubmit(values:Object):void {
     this.submitted = true;
     if (this.form.valid) {
       // your code goes here
-      this.af.auth.login({
-        email: this.email.value,
-        password: this.password.value
-      }).then(
-        () => this.router.navigate(['/pages/dashboard']))
-        .catch(error => console.log(error));
+    //   this.af.auth.login({
+    //     email: this.email.value,
+    //     password: this.password.value
+    //   }).then(
+    //     () => this.router.navigate(['/pages/dashboard']))
+    //     .catch(error => console.log(error));
+    console.log(this.client.value);
+    console.log(this.password.value);
+    console.log(this.email.value);
     }
+
+  }
+
+  public checkClient(): Boolean {
+    for (var i = 0; i < CLIENTS.length; i++)
+    {
+      if (this.client.value == CLIENTS[i]){
+        this.credentials = CLIENTS[i].credentials;
+        return true;
+      }
+    }
+    return false;
   }
 }
