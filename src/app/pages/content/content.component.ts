7552f2d191847194f9589293a76062fc9f6ded8c
Leonardo Antonio Santella <leocaracas2010@gmail.com>
2017-01-16 18:20:54 -0400

    fetching data. test purpose only

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { FirebaseService } from '../../app-firebase.service';
 
 @Component({
     selector: 'content-ma',
     template: `<strong> My page content here </strong>`
 })
 export class ContentComponent{
-    constructor() {}
+    public modules: Object;
+    constructor(private fs: FirebaseService) {}
+    ngOnInit(){
+        this.fs.db.ref('modules').on('value', (snapshot) => {
+            console.log(snapshot.val());
+            this.modules = snapshot.val();
+            console.log(this.modules);
+        });
+
+    }
+
+    public updateModule(val){
+        this.modules = val;
+    }
 }
 
