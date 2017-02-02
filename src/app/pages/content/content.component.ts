47b2c0bcda4bc08bc0aa1a80d11f436df1b09709
Leonardo Antonio Santella <leocaracas2010@gmail.com>
2017-02-02 08:03:52 -0400

    Modify and Delete

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -13,36 +13,76 @@ export class ContentComponent{
     private model: any;
     private columns: string[];
     private actions;
-    private banners = [];
+    private objects = [];
+    private toModify : any = undefined;
     constructor(
         private fs: FirebaseService,
         private router: Router,
         private route: ActivatedRoute) {}
+
     ngOnInit(){
         this.route.params.subscribe((params) => {
+            this.objects = [];
             this.module = params['id'];
             var module = this.fs.modules[this.module];
             this.schema = module.schemaForm.schema;
             this.columns = module.tableColumns;
             var buttons = [{id:"add", label:"Add", ngClass:"status-button btn btn-xs btn-success btn-lg"}]
-            this.actions = {
-                add: (property) => {
-                    var moduleRef = this.fs.db.ref().child(this.module);
-                    moduleRef.push(property.value);
+            this.actions = { add: (obj) => {
+                var moduleRef = this.fs.db.ref().child(this.module);
+                if (this.toModify == undefined){
+                    console.log("push", obj);
+                    moduleRef.push(obj.value).then(() =>  this.toModify = undefined);                   
+                }
+                else{
+                    console.log("update", this.toModify);
+                    var update = {};
+                    update['/' + this.toModify.key] = obj.value;
+                    moduleRef.update(update).then(() => {
+                        this.toModify = undefined;
+                        console.log("Modified");
+                    }); 
                 }
             }
+            };
             this.schema["buttons"] = buttons;
             var moduleRef = this.fs.db.ref().child(this.module);
-            // moduleRef.once('value').then((snapshot) => {
-            //     var banners = snapshot.val();
-            //     for (var key in banners){
-            //         if(banners.hasOwnProperty(key)){
-            //             this.banners.push(banners[key]);
-            //         }
-            //     }
-            //     console.log(this.banners)});
-            moduleRef.on('child_added', (data) => this.banners.push(data.val()));
+
+            moduleRef.on('child_added', (data) => {
+                console.log(data.key, data.val());
+                this.objects.push({key: data.key, value: data.val()})});
+            moduleRef.on('child_removed', (data) => {
+                var objects = [];
+                for (var o in this.objects){
+                    console.log(this.objects[o].key, data.key);
+                    if (this.objects[o].key != data.key)
+                    {
+                        objects.push(this.objects[o]);
+                    }
+                }
+                this.objects = objects;
+                console.log(this.objects);
+            });
+            moduleRef.on('child_changed', (data) =>{
+                for (var o in this.objects){
+                    console.log(this.objects[o].key, data.key);
+                    if (this.objects[o].key == data.key)
+                        this.objects[o].value = data.val();
+                }
+            });
         });
+    }
+
+    delete(obj){
+        var moduleRef = this.fs.db.ref().child(this.module+"/"+obj.key);
+        moduleRef.remove().then(() => this.toModify = undefined);
+
+    }
+
+    modify(obj){
+        this.model = obj.value;
+        this.toModify = obj;
+        console.log(this.toModify);
     }
 }
 
